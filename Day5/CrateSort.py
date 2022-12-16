ea1eeae67c44234dceb3ec415459d4217b233e85
Modena <brian.usage@gmail.com>
2022-12-15 23:36:46 -0800

    solution found, fixing tomorrow

--- Day5/CrateSort.py
+++ Day5/CrateSort.py
@@ -10,6 +10,8 @@ def logToString (content):
                 # remove spaces
                 if content[crate][stacks] != ' ':
                     stackLog += content[crate][stacks]
+    # bad way to do this, but adding 10 to help with stackSorter
+    stackLog += '10'
     return stackLog
 
 # a function that:
@@ -18,6 +20,13 @@ def logToString (content):
 # sort the stackLog based on the instructions
 # returns the new stackLog
 def stackSorter (oldLog, instruct):
+
+    # dumbass fix for when stack1 is empty and it identifies 10 as 1
+    if len(oldLog) > 67:
+        fixedLog = oldLog[:66]
+        fixedLog += '0'
+        oldLog = fixedLog
+
     fromPosition = instruct.find('from')
     toPosition = instruct.find('to')
     moveAmount = ''
@@ -32,13 +41,32 @@ def stackSorter (oldLog, instruct):
                 fromStack = instruct[number]
             elif number > toPosition:
                 toStack = instruct[number]
-    print(moveAmount, fromStack, toStack)
 
-    # find the charaters between fromStack and fromStack+1
-    #   i.e oldLog.find(fromStack) and find(str(int(fromStack)+1)
-    # copy the values to a variable
-    #   movingCrates = oldLog[fromStack(position)+1 : end(position)]
-    # 
+    # print(moveAmount,fromStack,toStack)
+    # Figure out how to move the crates around
+        # find the charaters between fromStack and fromStack+1
+        #   i.e oldLog.find(fromStack) and find(str(int(fromStack)+1)
+        # copy the values to a variable
+        #   movingCrates = oldLog[fromStack(position)+1 : end(position)]
+        # get the moving crate by providing the stackID and stackID + 1
+    movingCrates = oldLog[oldLog.find(fromStack):oldLog.find(str(int(fromStack)+1))]
+    remainingCrates = movingCrates[:-int(moveAmount)]
+    movedCrates = movingCrates[len(remainingCrates):]
+
+    # stores the modified log in a new log
+    newLog = oldLog.replace(movingCrates,remainingCrates)
+
+    # find the existingCrates
+    existingCrates = newLog[newLog.find(toStack):newLog.find(str(int(toStack)+1))]
+    # add the movedCrates to the existingCrates
+    updatedCrates = existingCrates + movedCrates
+    # stores the modified log in new log
+    newLog = newLog.replace(existingCrates, updatedCrates)
+    
+
+    print(newLog)
+    # return this newly sorted log
+    return newLog
 
 
 # a function that:
@@ -53,3 +81,10 @@ with open ('Cratelog.txt') as file:
     # to stackSorter, store the new log as stackLog
     for instruction in range (10,len(content)):
         stackLog = stackSorter(stackLog, content[instruction])
+    for char in range(1,len(stackLog)-1):
+        if ord(stackLog[char]) in range(48,58):
+            print(stackLog[char-1])
+
+# CRATES ARE MOVED ONE AT A TIME
+# THIS MEANS THAT THE ORDER IS REVERSED!
+# YOU DUMPASS
